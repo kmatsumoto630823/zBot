df81369d7cab1f10811c6d412992171a60b2ad49
kmatsumoto630823 <k.matsumoto.s630823@gmail.com>
2025-03-09 18:19:43 +0900

    軽微な修正

--- zBotTextToSpeech.js
+++ zBotTextToSpeech.js
@@ -11,7 +11,7 @@ const requiredEnvVars = [
 
 // 環境変数の存在チェック
 const missingEnvVars = requiredEnvVars.filter(envVar => !process.env[envVar] || process.env[envVar].trim() === "");
-if (missingEnvVars.length > 0) {
+if(missingEnvVars.length > 0){
     console.error(`Error: Missing required environment variables: ${missingEnvVars.join(", ")}`);
     process.exit(1); // エラー終了
 }
